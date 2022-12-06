c6b03a49413ab194a07e43087d42e99e4ae5e80a
Kyaw Thu <evanch98@gmail.com>
2022-12-06 19:51:55 +0800

    Interface for list

---
+++ src/List.java
@@ -0,0 +1,46 @@
+/**
+ * List ADT
+ */
+public interface List<E> {
+
+    /**
+     * Remove all contents from the list
+     */
+    public void clear();
+
+    /**
+     * Push an element at the start of the list
+     * @param item The element to be inserted
+     */
+    public void push(E item);
+
+    /**
+     * Append an element at the end of the list
+     * @param item The element to be inserted
+     */
+    public void append(E item);
+
+    /**
+     * Remove and return the removed element
+     * @param item The element to be removed
+     * @return The element that was removed
+     */
+    public E remove(E item);
+
+    /**
+     * Find the element in the list
+     * @param item The element to be searched
+     * @return true if the element is found, false otherwise
+     */
+    public boolean find(E item);
+
+    /**
+     * Traverse the list
+     */
+    public void traverse();
+
+    /**
+     * @return The number of element in the list
+     */
+    public int length();
+}
