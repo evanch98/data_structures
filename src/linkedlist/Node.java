cf2f3fbeeae465c968ebbef025e252b281faae79
Kyaw Thu <evanch98@gmail.com>
2022-12-06 20:18:45 +0800

    Adding the Node class for the LinkedList

---
+++ src/linkedlist/Node.java
@@ -0,0 +1,65 @@
+package linkedlist;
+
+/**
+ * Represent the node in the LinkedList
+ * @param <E> The data type of the node
+ */
+public class Node<E> {
+
+    private E data;    // data of the node
+    private Node<E> next;    // pointer to the next node
+
+    /**
+     * Empty constructor
+     */
+    Node(){}
+
+    /**
+     * Constructor with one parameter
+     * @param data Set the value of data
+     */
+    Node(E data) {
+        this.data = data;
+    }
+
+    /**
+     * Constructor with two parameters
+     * @param data Set the value of data
+     * @param next Set the value of next
+     */
+    Node(E data, Node<E> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    /**
+     * @return The data of the node
+     */
+    public E getData() {
+        return data;
+    }
+
+    /**
+     * To set the data of the node
+     * @param newData The value of the data
+     */
+    public void setData(E newData) {
+        data = newData;
+    }
+
+    /**
+     * @return The node next to the current node
+     */
+    public Node<E> getNext() {
+        return next;
+    }
+
+    /**
+     * To set the pointer to the other node
+     * @param newNext The node to be pointed to
+     */
+    public void setNext(Node<E> newNext) {
+        next = newNext;
+    }
+
+}
