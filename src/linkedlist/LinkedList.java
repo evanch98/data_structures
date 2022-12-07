2f57b1d1d507e02ebe0a212b00a4fc9e46a52618
Kyaw Thu <evanch98@gmail.com>
2022-12-08 02:02:20 +0800

    Implementing clear() and length() method

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -2,5 +2,18 @@ package linkedlist;
 
 public class LinkedList<E> implements List<E>{
 
+    private Node<E> root;
+    private int length = 0;
+
+    @Override
+    public void clear() {
+        root = null;
+    }
+
+    @Override
+    public int length() {
+        return length;
+    }
+
 
 }
