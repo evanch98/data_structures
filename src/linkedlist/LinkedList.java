6810e3a12b811e9108151df4ad13d49bc61ae716
Kyaw Thu <evanch98@gmail.com>
2022-12-08 17:45:52 +0800

    Add TODO lists

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -15,5 +15,25 @@ public class LinkedList<E> implements List<E>{
         return length;
     }
 
+    @Override
+    public void insert(E item) {
+        // TODO: Implement the method
+    }
 
+    @Override
+    public E remove(E item) {
+        // TODO: Implement the method
+        return null;
+    }
+
+    @Override
+    public boolean find(E item) {
+        // TODO: Implement the method
+        return false;
+    }
+
+    @Override
+    public void traverse() {
+        // TODO: Implement the method
+    }
 }
