347d1095041fa5c443b6954649b90b794fca6491
Kyaw Thu <evanch98@gmail.com>
2022-12-08 21:45:35 +0800

    Implement the insert method and change the variable name 'root' to 'head.'

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,13 +1,13 @@
 package linkedlist;
 
-public class LinkedList<E> implements List<E>{
+public class LinkedList<E extends Comparable<E>> implements List<E>{
 
-    private Node<E> root;
+    private Node<E> head;
     private int length = 0;
 
     @Override
     public void clear() {
-        root = null;
+        head = null;
     }
 
     @Override
@@ -16,8 +16,34 @@ public class LinkedList<E> implements List<E>{
     }
 
     @Override
+    // The insert method will always add an element in ascending order.
+    // Therefore, the list will always be sorted.
     public void insert(E item) {
-        // TODO: Implement the method
+        Node<E> newNode = new Node<>(item);  // a node to contain the new item
+
+        if (head == null) {
+            // The new item is the first item in the list.
+            head = newNode;
+        } else if (head.getData().compareTo(item) >= 0) {
+            // The new item is less than the head of the list; therefore, it becomes
+            // the root of the list
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            // The new item belongs somewhere after the first item
+            // Traverse the list to find its appropriate position
+            Node<E> runner = head.getNext();   // a node for traversing the list
+            Node<E> previous = head;           // always points to the node preceding runner
+            while (runner != null && runner.getData().compareTo(item) < 0) {
+                // Move the runner and previous along the list until the runner reach the end of
+                // the list or finds an element that is greater than or equal to the new item.
+                previous = runner;
+                runner = runner.getNext();
+            }
+            newNode.setNext(runner);
+            previous.setNext(newNode);
+            length++;    // increment the length of the list
+        }
     }
 
     @Override
