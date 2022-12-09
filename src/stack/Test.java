2547977f59fcedeb2f1c025c9c82c019fbf01930
Kyaw Thu <evanch98@gmail.com>
2022-12-10 02:02:19 +0800

    Testing Stack()

--- src/stack/Test.java
+++ src/stack/Test.java
@@ -1,2 +1,16 @@
-package stack;public class Test {
+package stack;
+
+public class Test {
+    public static void main(String[] args) {
+        Stack<Character> charStack = new Stack<>();
+        charStack.push('A');
+        charStack.push('B');
+        charStack.push('C');
+        charStack.traverse();
+        System.out.println(charStack.length());
+        charStack.pop();
+        System.out.println();
+        charStack.traverse();
+        System.out.println(charStack.length());
+    }
 }
