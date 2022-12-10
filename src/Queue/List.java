c83214eb8ac9ae541de41403f3a6c2c25f54c1ea
Kyaw Thu <evanch98@gmail.com>
2022-12-10 22:09:58 +0800

    List interface for the Queue

---
+++ src/Queue/List.java
@@ -0,0 +1,43 @@
+package Queue;
+
+/**
+ * List ADT
+ */
+public interface List<E extends Comparable<E>> {
+
+    /**
+     * Remove all contents from the list
+     */
+    public void clear();
+
+    /**
+     * Insert an element on top of the queue
+     * @param item The element to be inserted
+     */
+    public void enqueue(E item);
+
+    /**
+     * Remove the tail of the list and return the removed element
+     * @return The removed element
+     */
+    public E dequeue();
+
+    /**
+     * Find the element in the list
+     * @param item The element to be searched
+     * @return true if the element is found, false otherwise
+     */
+    public boolean find(E item);
+
+    /**
+     * Traverse the list
+     */
+    public void traverse();
+
+    /**
+     * @return The number of element in the list
+     */
+    public int length();
+}
+
+
