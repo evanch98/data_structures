4c3ad5604cd08bcdfbcfcf12f3b0abe635986886
Kyaw Thu <evanch98@gmail.com>
2022-12-10 22:47:04 +0800

    Implement enqueue method

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -3,19 +3,27 @@ package Queue;
 import linkedlist.Node;
 public class Queue<E extends Comparable<E>> implements List<E> {
 
-    private Node<E> top;
-    private Node<E> tail;
-    private int length = 0;
+    private Node<E> head;    // the first node in the queue
+    private Node<E> tail;    // the last node in the queue
+    private int length = 0;    // length of the queue
 
     @Override
     public void clear() {
-        top = null;
+        head = null;
         tail = null;
         length = 0;
     }
 
     @Override
     public void enqueue(E item) {
+        Node<E> newTail = new Node<>(item);
+
+        if (head == null) {
+            head = newTail;
+        } else {
+            tail.setNext(newTail);
+        }
+        tail = newTail;
 
     }
 
