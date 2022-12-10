ed892d36f4cf53acf68845e8fc40b5737b2b1439
Kyaw Thu <evanch98@gmail.com>
2022-12-11 01:13:07 +0800

    Test Queue

---
+++ src/Queue/Test.java
@@ -0,0 +1,17 @@
+package Queue;
+
+public class Test {
+    public static void main(String[] args) {
+        Queue<Double> queue = new Queue<Double>();
+        queue.enqueue(20.0);
+        queue.enqueue(30.0);
+        queue.enqueue(40.0);
+        queue.traverse();
+        System.out.println(queue.length());
+        System.out.println(queue.find(20.0));
+        queue.dequeue();
+        queue.traverse();
+        System.out.println(queue.length());
+        System.out.println(queue.find(20.0));
+    }
+}
