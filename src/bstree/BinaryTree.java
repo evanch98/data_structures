087f4e5c2278ff55b186bd36e76eb6b12b13bf84
Kyaw Thu <evanch98@gmail.com>
2022-12-11 23:18:27 +0800

    Interface for the binary tree

--- src/bstree/BinaryTree.java
+++ src/bstree/BinaryTree.java
@@ -2,4 +2,37 @@ package bstree;
 
 public interface BinaryTree<E extends Comparable<E>> {
 
+    /**
+     * Insert an element into the tree
+     * @param data The element to be inserted
+     * @return true if the element is inserted successfully
+     */
+    public boolean insert(E data);
+
+    /**
+     * @param data The element to be removed
+     * @return true if the element is removed successfully; false otherwise
+     */
+    public boolean delete(E data);
+
+    /**
+     * Traverse the tree
+     */
+    public void traverse();
+
+    /**
+     * @return The maximum value of the tree
+     */
+    public E getMax();
+
+    /**
+     * @return The minimum value of the tree
+     */
+    public E getMin();
+
+    /**
+     * @return true if the tree is empty; false otherwise
+     */
+    public boolean isEmpty();
+
 }
