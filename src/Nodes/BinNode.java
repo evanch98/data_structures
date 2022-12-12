106ad82290e2f09b9001cb88f49799ffec71485a
Kyaw Thu <evanch98@gmail.com>
2022-12-12 19:19:05 +0800

    Constructors for BinNode

--- src/Nodes/BinNode.java
+++ src/Nodes/BinNode.java
@@ -6,4 +6,16 @@ public class BinNode<E extends Comparable<E>> {
     private BinNode<E> leftChild;
 
     private BinNode<E> rightChild;
+
+    public BinNode() {}
+
+    public BinNode(E data) {
+        this.data = data;
+    }
+
+    public BinNode(E data, BinNode<E> leftChild, BinNode<E> rightChild) {
+        this.data = data;
+        this.leftChild = leftChild;
+        this.rightChild = rightChild;
+    }
 }
