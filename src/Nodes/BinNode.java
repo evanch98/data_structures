cafc608424e4b8eb065335ace53424b31dbe843b
Kyaw Thu <evanch98@gmail.com>
2022-12-12 19:22:31 +0800

    Add getter and setter methods

--- src/Nodes/BinNode.java
+++ src/Nodes/BinNode.java
@@ -18,4 +18,28 @@ public class BinNode<E extends Comparable<E>> {
         this.leftChild = leftChild;
         this.rightChild = rightChild;
     }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E newData) {
+        data = newData;
+    }
+
+    public BinNode<E> getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(BinNode<E> newChild) {
+        leftChild = newChild;
+    }
+
+    public BinNode<E> getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(BinNode<E> newChild) {
+        rightChild = newChild;
+    }
 }
