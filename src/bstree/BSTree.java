f61c1534e64479bdbf18ddc780a9d641495ddc9a
Kyaw Thu <evanch98@gmail.com>
2022-12-16 13:35:22 +0800

    Implement getMin() method

--- src/bstree/BSTree.java
+++ src/bstree/BSTree.java
@@ -28,7 +28,24 @@ public class BSTree<E extends Comparable<E>> implements BinaryTree<E> {
 
     @Override
     public E getMin() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        return getMin(root);
+    }
+
+    /**
+     * To find the minimum value of the tree
+     * @param node The node to be checked
+     * @return The value of the node
+     */
+    private E getMin(BinNode<E> node) {
+        // The minimum value of the binary tree resides at the left most node
+        if (node.getLeftChild() != null) {
+            // As long as the left child is not null, it is not the node with the minimum value
+            return getMin(node.getLeftChild());
+        }
+        return node.getData();
     }
 
     @Override
