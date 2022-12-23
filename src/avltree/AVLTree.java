3b05f8550a461cd709740b384086c934dd0c00c8
Kyaw Thu <evanch98@gmail.com>
2022-12-23 14:39:27 +0800

    Initialize AVL Tree class

---
+++ src/avltree/AVLTree.java
@@ -0,0 +1,36 @@
+package avltree;
+
+import bstree.BinaryTree;
+
+public class AVLTree<E extends Comparable<E>> implements BinaryTree<E> {
+
+    @Override
+    public void insert(E data) {
+
+    }
+
+    @Override
+    public void delete(E data) {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
+
+    @Override
+    public E getMax() {
+        return null;
+    }
+
+    @Override
+    public E getMin() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
