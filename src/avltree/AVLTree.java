966829a6a1a5dab6737d77070d1690d69e30240e
Kyaw Thu <evanch98@gmail.com>
2022-12-28 00:06:06 +0800

    Implement traverse, getMax, getMin, and isEmpty methods

--- src/avltree/AVLTree.java
+++ src/avltree/AVLTree.java
@@ -1,8 +1,11 @@
 package avltree;
 
+import Nodes.BinNode;
 import bstree.BinaryTree;
 
 public class AVLTree<E extends Comparable<E>> implements BinaryTree<E> {
+
+    private BinNode<E> root;
 
     @Override
     public void insert(E data) {
@@ -16,21 +19,70 @@ public class AVLTree<E extends Comparable<E>> implements BinaryTree<E> {
 
     @Override
     public void traverse() {
+        System.out.println("Preorder Traversal: ");
+        traversePreOrder(root);
+        System.out.println("In-order Traversal: ");
+        traverseInOrder(root);
+        System.out.println("Post-order Traversal: ");
+        traversePostOrder(root);
+    }
 
+    private void traversePreOrder(BinNode<E> node) {
+        if (node != null) {
+            System.out.println(node.getData());
+            traversePreOrder(node.getLeftChild());
+            traversePreOrder(node.getRightChild());
+        }
+    }
+
+    private void traverseInOrder(BinNode<E> node) {
+        if (node != null) {
+            traverseInOrder(node.getLeftChild());
+            System.out.println(node.getData());
+            traverseInOrder(node.getRightChild());
+        }
+    }
+
+    private void traversePostOrder(BinNode<E> node) {
+        if (node != null) {
+            traversePostOrder(node.getLeftChild());
+            traversePostOrder(node.getRightChild());
+            System.out.println(node.getData());
+        }
     }
 
     @Override
     public E getMax() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        return getMax(root);
+    }
+
+    private E getMax(BinNode<E> node) {
+        if (node.getRightChild() != null) {
+            return getMax(node.getRightChild());
+        }
+        return node.getData();
     }
 
     @Override
     public E getMin() {
-        return null;
+        if (isEmpty()) {
+            return null;
+        }
+        return getMin(root);
+    }
+
+    private E getMin(BinNode<E> node) {
+        if (node.getLeftChild() != null) {
+            return getMin(node.getLeftChild());
+        }
+        return node.getData();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return root == null;
     }
 }
