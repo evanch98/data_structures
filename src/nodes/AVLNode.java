c0d39bf9e1270cf5ed4cbf842ca620dd5ecc28d1
Kyaw Thu <evanch98@gmail.com>
2023-01-03 00:27:11 +0800

    Add constructors

--- src/nodes/AVLNode.java
+++ src/nodes/AVLNode.java
@@ -3,6 +3,14 @@ package nodes;
 public class AVLNode<E extends Comparable<E>> extends BinNode<E> {
     private int height = 0;
 
+    public AVLNode(E data) {
+        super(data);
+    }
+
+    public AVLNode(E data, AVLNode<E> leftChild, AVLNode<E> rightChild) {
+        super(data, leftChild, rightChild);
+    }
+
     public int getHeight() {
         return height;
     }
